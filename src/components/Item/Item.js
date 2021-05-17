ed7ae63db53670de6da19ac650723ebd071652d8
Nina Dorokhova <71547180+DorokhovaNA@users.noreply.github.com>
2021-05-17 21:22:03 +0300

    Initialize patch-16

---
+++ src/components/Item/Item.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+import Checkbox from '@material-ui/core/Checkbox';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import styles from './Item.module.css';
+
+const Item = ({ value, isDone, onClickDone, id, onClickDelete }) => (<div className = {
+    classnames({
+        [styles.item]: true,
+        [styles.done]: isDone
+    })
+}>
+    <Checkbox
+        checked={isDone}
+        defaultChecked
+        color="secondary"
+        inputProps={{ 'aria-label': 'secondary checkbox' }}
+        onClick={() => onClickDone(id)}
+     />
+     {value}
+     <Tooltip title="Delete">
+        <IconButton aria-label="delete" color="secondary" onClick={() => onClickDelete(id)}>
+          <DeleteIcon />
+        </IconButton>
+      </Tooltip>
+</div>);
+
+Item.propTypes = {
+	  isDone: PropTypes.bool,
+    value: PropTypes.string,
+    id: PropTypes.number
+};
+
+export default Item;
