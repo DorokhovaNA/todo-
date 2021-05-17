7a7f16a5ac758df36059d51cb89ab61093933d35
Nina Dorokhova <71547180+DorokhovaNA@users.noreply.github.com>
2021-05-17 21:22:03 +0300

    Initialize patch-14

---
+++ src/components/Item/Item.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import classnames from 'classnames';
+import Checkbox from '@material-ui/core/Checkbox';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import styles from './Item.module.css';
+
+const Item = ({ value, isDone, onClickDone, id, onClickDelete }) => (<div className = {
+    classnames({
+        [styles.item]: true,
+        [styles.done]: isDone
+    })
+}>
+    <Checkbox
+        defaultChecked
+        color="secondary"
+        inputProps={{ 'aria-label': 'secondary checkbox' }}
+        onClick={() => onClickDone(id)}
+     />
+     {value}
+     <Tooltip title="Delete">
+        <IconButton aria-label="delete" color="secondary" onClick={() => onClickDelete(id)}>
+          <DeleteIcon />
+        </IconButton>
+      </Tooltip>
+</div>);
+
+export default Item;
---
+++ src/components/ItemList/ItemList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Item from '../Item/Item';
+
+
+const ItemList = ({ items, onClickDone, onClickDelete }) => (<ul>
+  {items.map(item => <li key={item.value}>
+      <Item
+          value={item.value}
+          isDone = {item.isDone}
+          id={item.id}
+          onClickDone={onClickDone}
+          onClickDelete={onClickDelete}
+      />
+  </li>)}
+</ul>);
+
+export default ItemList;
