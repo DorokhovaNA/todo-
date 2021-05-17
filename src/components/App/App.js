9ef98b5887323e19cc47d790e601cf4f2c90a053
Nina Dorokhova <71547180+DorokhovaNA@users.noreply.github.com>
2021-05-17 21:22:02 +0300

    Initialize patch12

---
+++ src/components/App/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ItemList from '../ItemList/ItemList';
+import InputItem from '../InputItem/InputItem';
+import Footer from '../Footer/Footer';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import styles from './App.module.css';
+
+class App extends React.Component {
+  state = {
+    items: [
+        {
+          value: 'Проанализировать партнерский и зарплатный канал',
+          isDone: true
+        },
+        {
+          value: 'Учесть конвертацию заявок в разрезе каждого канала',
+          isDone: false
+        },
+        {
+          value: 'Подготовить аналитическую отчетность',
+          isDone: true
+        }
+    ]
+  };
+  render() {
+    return (
+    <div className={styles.wrap}>
+        <Card variant="outlined">
+            <CardContent>
+                <h1 className={styles.title}>Важные дела:</h1>
+                <InputItem />
+                <ItemList items={this.state.items} />
+                <Footer count={3} />
+            </CardContent>
+        </Card>
+    </div>);
+  }
+};
+
+export default App;
